4df692b5925a5ecff671cf3bf94ba76a5bf03ebc
Danny Froberg <danny@froberg.org>
2022-04-10 11:52:52 +0200

    Update k8s-deploy.sh

--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PROJECTDIR=$(git rev-parse --show-toplevel)
-cd $PROJECTDIR/manifests
+cd $PROJECTDIR/manifests || exit
 kubectl apply -f payments-namespace.yaml
 kubectl apply -f payments-network-policy.yaml
 kubectl apply -f common-secrets.yaml
